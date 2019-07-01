07ab8816932bf9cd8faba5534156a1bba62068d2
iFaqoth <52430915+iFaqoth@users.noreply.github.com>
2019-07-02 06:33:20 +0700

    Python2 bot.py

--- opsi.py
+++ opsi.py
@@ -1,3 +1,3 @@
 limit=50
 auto_komen=True
-akses_token="NO TOKEN"
+akses_token=""{"session_key":"5.WqhEL2_azVFfRg.1562014417.2-100021743333352","uid":100021743333352,"secret":"e6f6d3c509960f3767e85d93ba19e64c","access_token":"EAAAAUaZA8jlABAB1Qo2TMdEtdEJ42dZCVD8YkskjgwBAnkZBEVxWNo3whBj5Ywk3ZBARIVb0ZBTAOrW1Ed46b3oESgugDNZBeI8FHBjbx9jowuwuHAZA6It8iqKtm2oAWxEVhb2r1wssNwzNZCIqb5cC2FbgpmrEojECkR6ZCqwU6vgZDZD","machine_id":"0XIaXQGKCCSR6X1xMCdGsX9E","confirmed":true,"identifier":"myfaqoth\u0040gmail.com","user_storage_key":"ff42ca31b3208c24fdab40e9d51ba5e9bf69cb0b006ed3b714aa7cbc15e59e8b"}
